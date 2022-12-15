52fdd5d1ef3ba62bd2542f364ec46728f0edce22
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-15 10:33:05 +0100

    step 3: extract approval logic to Order -> simplify approval / rejection logic

--- scala/src/main/scala/ordershipping/domain/Order.scala
+++ scala/src/main/scala/ordershipping/domain/Order.scala
@@ -1,34 +1,50 @@
 package ordershipping.domain
 
 import ordershipping.domain.OrderItem.createOrderItem
-import ordershipping.domain.OrderStatus.{Approved, Created, OrderStatus, Rejected, Shipped}
+import ordershipping.domain.OrderStatus.{
+  Approved,
+  Created,
+  OrderStatus,
+  Rejected,
+  Shipped
+}
 // TODO : this layer should not access those exceptions -> move them inside Domain
-import ordershipping.usecase.{ApprovedOrderCannotBeRejectedException, RejectedOrderCannotBeApprovedException, ShippedOrdersCannotBeChangedException}
+import ordershipping.usecase.{
+  ApprovedOrderCannotBeRejectedException,
+  RejectedOrderCannotBeApprovedException,
+  ShippedOrdersCannotBeChangedException
+}
 
 // TODO : make it private
 class Order(
-             var currency: String = "",
-             var items: List[OrderItem] = List.empty,
-             var status: OrderStatus,
-             var id: Int
-           ) {
-  def approve(): Unit = {
-    // TODO : check status duplicated between approve and reject
-    if (status == Shipped)
-      throw new ShippedOrdersCannotBeChangedException
-    if (status == Rejected)
-      throw new RejectedOrderCannotBeApprovedException
+    var currency: String = "",
+    var items: List[OrderItem] = List.empty,
+    var status: OrderStatus,
+    var id: Int
+) {
 
-    status = Approved
+  def approve(): Unit = {
+    ifNotShipped { order =>
+      order.status match {
+        case Rejected => throw new RejectedOrderCannotBeApprovedException
+        case _        => order.status = Approved
+      }
+    }
   }
 
   def reject(): Unit = {
+    ifNotShipped { order =>
+      order.status match {
+        case Approved => throw new ApprovedOrderCannotBeRejectedException
+        case _        => order.status = Rejected
+      }
+    }
+  }
+
+  private def ifNotShipped(continueWith: Order => Unit): Unit = {
     if (status == Shipped)
       throw new ShippedOrdersCannotBeChangedException
-    if (status == Approved)
-      throw new ApprovedOrderCannotBeRejectedException
-
-    status = Rejected
+    continueWith(this)
   }
 
   def total: Double = items.map(i => i.taxedAmount).sum
