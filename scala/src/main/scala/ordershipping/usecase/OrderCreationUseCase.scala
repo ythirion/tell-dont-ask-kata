c508c454f67a03dd6d8fd1e095368b0e2887d4ad
Yoan Thirion <yoan.thirion@gmail.com>
2022-12-15 10:28:11 +0100

    step 2: enrich Order entity -> simplify creation use case

--- scala/src/main/scala/ordershipping/usecase/OrderCreationUseCase.scala
+++ scala/src/main/scala/ordershipping/usecase/OrderCreationUseCase.scala
@@ -1,30 +1,37 @@
 package ordershipping.usecase
 
-import ordershipping.domain.Order
+import ordershipping.domain.{Order, Product}
 import ordershipping.repository.{OrderRepository, ProductCatalog}
 
 class OrderCreationUseCase(
-                            val orderRepository: OrderRepository,
-                            val productCatalog: ProductCatalog
-                          ) {
+    val orderRepository: OrderRepository,
+    val productCatalog: ProductCatalog
+) {
   def run(request: SellItemsRequest): Unit = {
-    // TODO : simplify it -> split
-    val items = request.requests
-      .map(request => {
-        productCatalog.getByName(request.productName) match {
-          case Some(p) => (p, request.quantity)
-          case _ => throw new UnknownProductException
-        }
-      })
-      .toMap
-
-    val order = Order.create(items)
-
-    orderRepository.save(order)
+    orderRepository.save(
+      Order.create(
+        toProductQuantities(request)
+      )
+    )
   }
 
+  private def toProductQuantities(
+      request: SellItemsRequest
+  ): Map[Product, Int] =
+    request.requests
+      .map(toProductQuantity)
+      .toMap
+
+  private def toProductQuantity(
+      sellItemRequest: SellItemRequest
+  ): (Product, Int) =
+    productCatalog.getByName(sellItemRequest.productName) match {
+      case Some(p) => (p, sellItemRequest.quantity)
+      case _       => throw new UnknownProductException
+    }
+
   private def roundAt(p: Int)(n: Double): Double = {
-    val s = math pow(10, p)
+    val s = math pow (10, p)
     (math round n * s) / s
   }
 }
